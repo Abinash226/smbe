83d25fbd1ef0c4e6ea0c1d3f5db243e90979fc15
abinashmohanty <abinashm226@gmail.com>
2025-07-05 10:20:01 +0530

    First Commit

---
+++ src/main/java/com/ab/smbe/SmbeApplication.java
@@ -0,0 +1,13 @@
+package com.ab.smbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmbeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ab/smbe/SmbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.smbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
